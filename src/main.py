c91abd7e985622c2fb923fac4c16b2fc93bc4ffb
Rapunzel <osina_kv@mail.ru>
2025-03-15 19:24:55 +0300

    tests for str, add, iter, next, readme update, pep

--- src/main.py
+++ src/main.py
@@ -45,12 +45,14 @@ class Product:
         else:
             self.__price = new_price
             print(f'Цена успешно изменена на {self.__price} руб.')
+
     def __str__(self):
         return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
 
     def __add__(self, other):
         total = self.__price * self.quantity + other.price * other.quantity
         return total
+
 
 class Category:
     """Класс для подсчета количества товаров и категорий указанных продуктов"""
@@ -67,7 +69,6 @@ class Category:
         self.product_count = len(self.__products)
         Category.product_count += self.product_count
         Category.category_count += 1
-
 
     def __str__(self):
         total_quantity = sum(product.quantity for product in self.__products)
@@ -119,6 +120,7 @@ def create_objects_from_json(data: dict) -> list:
                                    products=products))
     return categories
 
+
 class ProductIterator:
     """Класс для перебора продуктов"""
     def __init__(self, category):
@@ -130,12 +132,13 @@ class ProductIterator:
         return self
 
     def __next__(self):
-        if self.index < len(self.category.products):
-            product = self.category.products[self.index]
+        if self.index < len(self.category._Category__products):
+            product = self.category._Category__products[self.index]
             self.index += 1
             return product
         else:
             raise StopIteration
+
 
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
