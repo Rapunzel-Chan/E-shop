f6effc6c8c09fb23483b73e789e521954f172791
Rapunzel <osina_kv@mail.ru>
2025-03-21 21:25:45 +0300

    tests for cls Smartphone & LawnGrass, pep, review README

--- src/main.py
+++ src/main.py
@@ -141,7 +141,9 @@ class ProductIterator:
         else:
             raise StopIteration
 
+
 class Smartphone(Product):
+    """Класс для подсчета Смартфонов - класс-наследник Product"""
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
@@ -155,7 +157,9 @@ class Smartphone(Product):
             return total
         raise TypeError
 
+
 class LawnGrass(Product):
+    """Класс для подсчета Травы газонной - класс-наследник Product"""
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         super().__init__(name, description, price, quantity)
         self.country = country
@@ -167,6 +171,7 @@ class LawnGrass(Product):
             total = self.price * self.quantity + other.price * other.quantity
             return total
         raise TypeError
+
 
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
@@ -223,10 +228,13 @@ if __name__ == "__main__":
     for product in categories_data:
         print(product)
 
-    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
-                         "S23 Ultra", 256, "Серый")
-    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
-    smartphone3 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера",
+                             180000.0, 5, 95.5,
+                             "S23 Ultra", 256, "Серый")
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8,
+                             98.2, "15", 512, "Gray space")
+    smartphone3 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14,
+                             90.3, "Note 11", 1024, "Синий")
 
     print(smartphone1.name)
     print(smartphone1.description)
@@ -255,8 +263,10 @@ if __name__ == "__main__":
     print(smartphone3.memory)
     print(smartphone3.color)
 
-    grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
-    grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+    grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0,
+                       20, "Россия", "7 дней", "Зеленый")
+    grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0,
+                       15, "США", "5 дней", "Темно-зеленый")
 
     print(grass1.name)
     print(grass1.description)
@@ -287,8 +297,10 @@ if __name__ == "__main__":
     else:
         print("Не возникла ошибка TypeError при попытке сложения")
 
-    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
-    category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
+    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны",
+                                    [smartphone1, smartphone2])
+    category_grass = Category("Газонная трава", "Различные виды газонной травы",
+                              [grass1, grass2])
 
     category_smartphones.add_product(smartphone3)
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.main import Category, Product
+from src.main import Category, LawnGrass, Product, Smartphone
 
 
 @pytest.fixture
@@ -48,3 +48,27 @@ def test_data():
 @pytest.fixture
 def product():
     return Product(name='Test Product', description='Test Description', price=100.0, quantity=10)
+
+
+@pytest.fixture
+def product_smartphone1():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0,
+                      5, 95.5,
+                      "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def product_smartphone2():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0,
+                      8, 98.2,
+                      "15", 512, "Gray space")
+
+
+@pytest.fixture
+def product_lawngrass1():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture
+def product_lawngrass2():
+    return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
